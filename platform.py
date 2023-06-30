901a5eab44fd5bd5e9f97a0da52e75fdd8869208
Jose Daniel Az <josedanielxela@gmail.com>
2023-06-29 21:35:31 -0600

    imagen alpha

--- platform.py
+++ platform.py
@@ -4,7 +4,7 @@ import pygame
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, image_path):
         super().__init__()
-        self.original_image = pygame.image.load(image_path).convert()
+        self.original_image = pygame.image.load(image_path).convert_alpha()
         self.image = pygame.transform.scale(self.original_image, (self.original_image.get_width() // 1,
                                                                   self.original_image.get_height() // 1))
         self.rect = self.image.get_rect()
