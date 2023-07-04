41f5e3cbb00f1c02cde4d78ce1f599c257c66ba2
Jose Daniel Az <josedanielxela@gmail.com>
2023-07-03 21:02:22 -0600

    platforms

--- play.py
+++ play.py
@@ -1,10 +1,6 @@
 import pygame
 import sys
 from pygame import *
-
-from Screen import Screen
-from Colisioner import Colisioner
-
 from CharacterDecorator.SpriteCharacter import SpriteCharacter
 from CharacterDecorator.HorizontalCharacter import HorizontalCharacter
 from CharacterDecorator.VerticalCharacter import VerticalCharacter
@@ -26,31 +22,39 @@ class Play:
         personaje = ControlCharacter(VerticalCharacter(HorizontalCharacter(personaje)), K_w, K_s, K_a, K_d)
         personaje1 = ControlCharacter(VerticalCharacter(HorizontalCharacter(personaje1)), K_UP, K_DOWN, K_LEFT, K_RIGHT)
         paredes = [pygame.Rect(0, 500, 960, 50),
-                   pygame.Rect(270, 350, 170, 50), pygame.Rect(570, 350, 170, 50),
-                   pygame.Rect(65, 100, 170, 50), pygame.Rect(800, 100, 170, 50),
-                   pygame.Rect(300, -150, 170, 50), pygame.Rect(700, -150, 170, 50),
-                   pygame.Rect(250, -350, 170, 50), pygame.Rect(625, -350, 170, 50),
-                   pygame.Rect(250, -550, 170, 50), pygame.Rect(625, -550, 170, 50),
-                   pygame.Rect(250, -650, 170, 50), pygame.Rect(625, -650, 170, 50)]
+                   pygame.Rect(100, 350, 170, 50), pygame.Rect(390, 350, 170, 50), pygame.Rect(680, 350, 170, 50),
+                   pygame.Rect(230, 125, 170, 50), pygame.Rect(560, 125, 170, 50),
+                   pygame.Rect(100, -100, 170, 50), pygame.Rect(390, -100, 170, 50), pygame.Rect(680, -100, 170, 50),
+                   pygame.Rect(230, -325, 170, 50), pygame.Rect(560, -325, 170, 50),
+                   pygame.Rect(100, -550, 170, 50), pygame.Rect(390, -550, 170, 50), pygame.Rect(680, -550, 170, 50),
+                   pygame.Rect(230, -775, 170, 50), pygame.Rect(560, -775, 170, 50),
+                   pygame.Rect(390, -960, 170, 50)
+                   ]
         kill_collide = [pygame.Rect(0, 500, 960, 2)]
-        up = down = left = rigth = False
+        up = down = left = right = False
         distancia_salto = 0
         doble_salto = True
+        stop_platforms: bool = False
 
         index = 0
         img_play = Img(self.screen)
+        img_flag = Img(self.screen)
         y_pos = -1080
         vel = 5
         collide_platform = CollidePlatform('img/map/platform(0.2)col.png', self.screen, paredes)
         while True:
             img_play.add_img('img/map/map.png', 0, y_pos, 1, 1)
+            img_flag.add_img('img/map/finish.png', 360, y_pos-5, 0.45, 0.45)
             eventos = pygame.event.get()
             personaje.control_move(eventos)
             personaje1.control_move(eventos)
             personaje.move(paredes)
             personaje1.move(paredes)
+
+            if stop_platforms:
+                vel = 0
+
             collide_platform.draw_platform(vel)
-            #pygame.draw.rect(ventana.view, (255, 0, 0), personaje.get_hitbox())
             self.screen.blit(personaje.get_actual_frame(), (personaje.get_x_coordinate(), personaje.get_y_coordinate()))
             self.screen.blit(personaje1.get_actual_frame(), (personaje1.get_x_coordinate(), personaje1.get_y_coordinate()))
             for evento in eventos:
@@ -66,5 +70,6 @@ class Play:
             reloj = pygame.time.Clock()
             if y_pos < 0:
                 y_pos += vel
+            else:
+                stop_platforms = True
             reloj.tick(40)
-
