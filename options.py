c7b110c066ae6811b51b689aa6addc407937d4d3
Rodolfoba1 <38680428+Rodolfoba1@users.noreply.github.com>
2023-07-04 03:05:34 -0600

    instructions text

--- options.py
+++ options.py
@@ -23,7 +23,17 @@ class Options:
             # add background image
             img_menu.add_img('img/background/menu_bg.png', 0, 0, 1, 1)
             # add 'menu' text on menu
-            pixel_label.draw_text(self.screen, "Opciones", (255, 255, 255), 360, 100, 120)
+            pixel_label.draw_text(self.screen, "Instrucciones", (255, 255, 255), 200, 100, 120)
+            pixel_label.draw_text(self.screen, "Jugador 1", (255, 255, 255), 70, 210, 80)
+            pixel_label.draw_text(self.screen, "Tecla W: Arriba ", (255, 255, 255), 50, 280, 60)
+            pixel_label.draw_text(self.screen, "Tecla A: Abajo ", (255, 255, 255), 50, 320, 60)
+            pixel_label.draw_text(self.screen, "Tecla S: Izquierda ", (255, 255, 255), 50, 360, 60)
+            pixel_label.draw_text(self.screen, "Tecla D: Derecha ", (255, 255, 255), 50, 400, 60)
+            pixel_label.draw_text(self.screen, "Tecla G: Golpe ", (255, 255, 255), 50, 440, 60)
+            pixel_label.draw_text(self.screen, "Jugador 2", (255, 255, 255), 550, 210, 80)
+            pixel_label.draw_text(self.screen, "Mover con flechas", (255, 255, 255), 530, 280, 60)
+            pixel_label.draw_text(self.screen, "del teclado. ", (255, 255, 255), 530, 320, 60)
+            pixel_label.draw_text(self.screen, "Tecla K: Golpe ", (255, 255, 255), 530, 360, 60)
             if menu.draw_button():
                 break
             for event in pygame.event.get():
